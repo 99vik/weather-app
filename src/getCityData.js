377b05506c12b8975925743569b4151ed117ea96
Viktor Miric <viktorm47@gmail.com>
2023-08-18 10:54:14 +0200

    Created API with console output

---
+++ src/getCityData.js
@@ -0,0 +1,10 @@
+export default async function getCityData(cityName) {
+  const cityNameLower = cityName.toLowerCase();
+  const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=63b8bf7c3cd84359870151427230808&q=${cityNameLower}&days=3&aqi=no&alerts=no`);
+  const responseData = await response.json();
+  if (responseData.error) {
+    console.log(responseData.error.message);
+  } else {
+    console.log(responseData);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './styles.css';
+import getCityData from './getCityData';
 
-console.log('hello world');
+const cityInputBtn = document.querySelector('#cityInputBtn');
+const cityInput = document.querySelector('#cityInput');
+cityInputBtn.addEventListener('click', () => {
+  getCityData(cityInput.value);
+});
