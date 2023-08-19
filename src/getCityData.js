36113d5f0d951f813e40c17a9634ac23d250f928
Viktor Miric <viktorm47@gmail.com>
2023-08-19 17:25:27 +0200

    Added loading animation

--- src/getCityData.js
+++ src/getCityData.js
@@ -1,9 +1,13 @@
 import displayData from "./displayData";
 
 export default async function getCityData(cityName) {
+  const main = document.querySelector('main');
+    main.innerHTML = '<div class="lds-ring"><div></div><div></div><div></div><div></div></div>';
   const cityNameLower = cityName.toLowerCase();
   const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=63b8bf7c3cd84359870151427230808&q=${cityNameLower}&days=3&aqi=no&alerts=no`);
   const responseData = await response.json();
+
+
   if (responseData.error) {
     alert(responseData.error.message);
   } else {
