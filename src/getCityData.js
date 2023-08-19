cd55dfb4bd9482c05e4125af8b4fc2e1d48a23d8
Viktor Miric <viktorm47@gmail.com>
2023-08-19 18:25:08 +0200

    Fixed https request bug in gh-pages

--- src/getCityData.js
+++ src/getCityData.js
@@ -4,7 +4,7 @@ export default async function getCityData(cityName) {
   const main = document.querySelector('main');
   main.innerHTML = '<div class="lds-ring"><div></div><div></div><div></div><div></div></div>';
   const cityNameLower = cityName.toLowerCase();
-  const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=63b8bf7c3cd84359870151427230808&q=${cityNameLower}&days=3&aqi=no&alerts=no`, {
+  const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=63b8bf7c3cd84359870151427230808&q=${cityNameLower}&days=3&aqi=no&alerts=no`, {
     mode: 'cors'
   });
   const responseData = await response.json();
