12503fc146cc6c7309d9cf8f28d20ed97ef1d308
Viktor Miric <viktorm47@gmail.com>
2023-08-19 14:33:57 +0200

    Added forecast divs for 3 days

--- src/displayData.js
+++ src/displayData.js
@@ -26,11 +26,47 @@ function createCurrentWeatherDiv(data) {
   return currentWeatherDiv;
 }
 
+function createForecastDayDiv(day) {
+  const div = document.createElement('div');
+  const dayDiv = document.createElement('p');
+
+  const daysOfWeek = ['Monday', 'Thuesday', 'wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  dayDiv.textContent = `${daysOfWeek[new Date(day.date).getDay()]}, ${new Date(day.date).getDate()}.     ${months[new Date(day.date).getMonth()]} `;
+
+  const tempDiv = document.createElement('div');
+  const img = document.createElement('img');
+  const tempText = document.createElement('p');
+  img.src = day.day.condition.icon;
+  tempText.textContent = `${Math.round(day.day.maxtemp_c)}° / ${Math.round(day.day.mintemp_c)}°`;
+  tempDiv.appendChild(tempText);
+  tempDiv.appendChild(img);
+
+  const text = document.createElement('p');
+  text.textContent = day.day.condition.text;
+
+  console.log(day);
+  div.appendChild(dayDiv);
+  div.appendChild(tempDiv);
+  div.appendChild(text);
+  return div;
+}
+
+function createForecastDiv(data) {
+  const forecastDiv = document.createElement('div');
+  forecastDiv.classList.add('forecast');
+
+  data.forecast.forecastday.forEach((day) => {
+    forecastDiv.appendChild(createForecastDayDiv(day));
+  });
+
+  return forecastDiv;
+}
+
 export default function displayData(data) {
   const main = document.querySelector('main');
   main.innerHTML = '';
 
   main.appendChild(createCurrentWeatherDiv(data));
-
-  console.log(data);
+  main.appendChild(createForecastDiv(data));
 }
